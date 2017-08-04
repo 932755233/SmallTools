acf4918483cb71433a525cc5e185f6e236d7889d
Denny <932755233@github.com>
2017-08-04 20:18:37 +0800

    main更改

--- java_test/src/com/zzy/Main.java
+++ java_test/src/com/zzy/Main.java
@@ -1,8 +1,20 @@
 package com.zzy;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    public Main() throws HeadlessException {
+
+        this.setTitle("测试");
+
+
+
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        new Main();
     }
 }
